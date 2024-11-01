8aa5af387354e5113d694c77055fd57dd1e0a268
dothaofpt <143236377+dothaofpt@users.noreply.github.com>
2024-11-01 12:02:22 +0700

    up

---
+++ src/test/java/org/example/projectbackend/ProjectBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
